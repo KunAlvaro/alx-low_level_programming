c7ee75ccb12053f61c84c3b9b4d011fdda64431b
KunAlvaro <alvarorocks17@gmail.com>
2022-09-08 18:34:58 -0700

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,18 +3,16 @@
 
 
 /**
- *
- * main - Entry point
- *
- * Return: Always 0 (Success)
- *
- * /
-int main(void)
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 
+
+
+int main(void)
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
