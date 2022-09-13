73850de78b827a8b603d7d169e9daf2a86ea6a03
KunAlvaro <alvarorocks17@gmail.com>
2022-09-13 15:18:11 -0700

    correction to task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,27 +18,23 @@ void print_times_table(int n)
 			{
 				product = (row * column);
 				if (column == 0)
-
 					_putchar('0' + product);
 				else
 				{
 					_putchar(',');
 					_putchar(' ');
-
 					if (product <= 9)
 					{
 						_putchar(' ');
 						_putchar(' ');
 						_putchar('0' + product);
 					}
-
 					else if (product > 9 && product < 100)
 					{
 						_putchar(' ');
 						_putchar('0' + (product / 10));
 						_putchar('0' + (product % 10));
 					}
-
 					else if (product >= 100)
 					{
 						_putchar('0' + (product / 100));
@@ -47,7 +43,6 @@ void print_times_table(int n)
 					}
 				}
 			}
-
 			_putchar('\n');
 		}
 	}
